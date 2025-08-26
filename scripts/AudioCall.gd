9748bf0f88f5c7e7c23b65bedef873134aa7fae5
Gauklereie <71492074+Gauklereie@users.noreply.github.com>
2025-08-26 23:57:32 +0200

    Add AudiocallManager scene and audio call logic
    Introduces the AudiocallManager scene with an AudioStreamPlayer node and links it to the new AudioCall.gd script. The AudioCall.gd script now manages audio call playback using a configurable dictionary of audio file paths. Minor adjustments were made to solution_box.tscn and testmain.tscn for node configuration.

--- scripts/AudioCall.gd
+++ scripts/AudioCall.gd
@@ -1,1 +1,34 @@
 extends Node
+
+@export var AudioCallConfig: Dictionary = {
+	101:{
+		0: "res://assets/sounds/Calls/Telefonseelsorge/Telefonseelsorge spricht 1.ogg",
+		1: "res://assets/sounds/Calls/Telefonseelsorge/Nora spricht 1.ogg",
+		2: "res://assets/sounds/Calls/Telefonseelsorge/Telefonseelsorge spricht 2.ogg",
+		3: "res://assets/sounds/Calls/Telefonseelsorge/Nora spricht 2.ogg",
+		4: "res://assets/sounds/Calls/Telefonseelsorge/Telefonseelsorge spricht 3.ogg",
+		5: "res://assets/sounds/Calls/Telefonseelsorge/Nora spricht 3.ogg",
+		6: "res://assets/sounds/Calls/Telefonseelsorge/Telefonseelsorge spricht 4.ogg",
+		7: "res://assets/sounds/Calls/Telefonseelsorge/Nora spricht 4.ogg",
+		8: "res://assets/sounds/Calls/Telefonseelsorge/Telefonseelsorge spricht 5.ogg",
+		9: "res://assets/sounds/Calls/Telefonseelsorge/Nora spricht 5.ogg",
+		10: "res://assets/sounds/Calls/Telefonseelsorge/Telefonseelsorge spricht 6.ogg",
+		11: "res://assets/sounds/Calls/Telefonseelsorge/Nora spricht 6.ogg",
+		12: "res://assets/sounds/Calls/Telefonseelsorge/Telefonseelsorge spricht 7.ogg",
+		13: "res://assets/sounds/Calls/Telefonseelsorge/Nora spricht 7.ogg",
+		14: "res://assets/sounds/Calls/Telefonseelsorge/Telefonseelsorge spricht 8.ogg"
+	},	
+	102:{
+		
+	}
+}
+	
+func _ready():
+	playCall(101,1)
+
+func playCall(call:int, zeile: int):
+	var AudioPath = AudioCallConfig[call].get(zeile)
+	var Audio = load(AudioPath);
+	%AudioStreamPlayerCALLER.stream = Audio
+	%AudioStreamPlayerCALLER.play()
+	
