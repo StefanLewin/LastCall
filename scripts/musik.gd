b21d3c4f3a5255aa44e26d754670b4f0a0b691df
RaiLappi <raimeyke@gmail.com>
2025-08-26 18:23:17 +0200

    musik manageradded

--- scripts/musik.gd
+++ scripts/musik.gd
@@ -1,7 +1,8 @@
 extends AudioStreamPlayer2D
 
 func fade_in():
+	print("x")
 	var tween = get_tree().create_tween()
-	volume_db = -20
+	volume_db = -10
 	play()
-	tween.tween_property(self, "volume_db", 0, 3)
+	tween.tween_property(self, "volume_db", 20, 3)
